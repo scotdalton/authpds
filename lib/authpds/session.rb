7205ebba023b21c7037904691537fed70cb29204
Scot Dalton <scotdalton@gmail.com>
2012-09-05 14:27:46 -0400

    Skip inaccessible check if session id cookie is missing.

--- lib/authpds/session.rb
+++ lib/authpds/session.rb
@@ -178,7 +178,9 @@ module Authpds
       end
 
       def authenticate
-        return false if controller.cookies["#{self.class.calling_system}_inaccessible".to_sym] == session_id
+        # Don't authenticate if the system is inaccessible.
+        # If the application session id is nil, skip this check.
+        return false if controller.cookies["#{self.class.calling_system}_inaccessible".to_sym] == session_id unless session_id.nil?
         # If PDS session already established, authenticate
         return true unless pds_user.nil?
         # Establish a PDS session if the user logged in via an alternative SSO mechanism and this isn't being called after login
